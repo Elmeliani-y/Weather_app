20bcfeef76031c0a0d4da4e36163aefde4b3659c
Elmeliani-y <119269216+Elmeliani-y@users.noreply.github.com>
2023-10-01 17:07:45 +0100

    Update Weatherapp.jsx

--- src/weather-app/weather-app/Weatherapp.jsx
+++ src/weather-app/weather-app/Weatherapp.jsx
@@ -24,39 +24,39 @@ const Weather = () => {
     location[0].innerHTML = data.name;
 
     if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
-      setWicon("./weather-app/assets/clear.png");
+      setWicon("https://github.com/Elmeliani-y/Weather_app/tree/main/src/weather-app/assets/clear.png");
     } else if (
       data.weather[0].icon === "02d" ||
       data.weather[0].icon === "02n"
     ) {
-      setWicon("./weather-app/assets/cloud.png");
+      setWicon("https://github.com/Elmeliani-y/Weather_app/tree/main/src/weather-app/assets/cloud.png");
     } else if (
       data.weather[0].icon === "03d" ||
       data.weather[0].icon === "03n"
     ) {
-      setWicon("./weather-app/assets/drizzle.png");
+      setWicon("https://github.com/Elmeliani-y/Weather_app/tree/main/src/weather-app/assets/drizzle.png");
     } else if (
       data.weather[0].icon === "04d" ||
       data.weather[0].icon === "04n"
     ) {
-      setWicon("./weather-app/assets/drizzle.png");
+      setWicon("https://github.com/Elmeliani-y/Weather_app/tree/main/src/weather-app/assets/drizzle.png");
     } else if (
       data.weather[0].icon === "09d" ||
       data.weather[0].icon === "09n"
     ) {
-      setWicon("./weather-app/assets/rain.png");
+      setWicon("https://github.com/Elmeliani-y/Weather_app/tree/main/src/weather-app/assets/rain.png");
     } else if (
       data.weather[0].icon === "10d" ||
       data.weather[0].icon === "10n"
     ) {
-      setWicon("./weather-app/assets/rain.png");
+      setWicon("https://github.com/Elmeliani-y/Weather_app/tree/main/src/weather-app/assets/rain.png");
     } else if (
       data.weather[0].icon === "13d" ||
       data.weather[0].icon === "13n"
     ) {
-      setWicon("./weather-app/assets/snow.png");
+      setWicon("https://github.com/Elmeliani-y/Weather_app/tree/main/src/weather-app/assets/snow.png");
     } else {
-      setWicon("./weather-app/assets/clear.png");
+      setWicon("https://github.com/Elmeliani-y/Weather_app/tree/main/src/weather-app/assets/clear.png");
     }
   };
 
@@ -82,7 +82,7 @@ const Weather = () => {
             </div>
           </div>
           <div className="weather-image text-center">
-            <img src={wicon} height={70} alt="Weather Icon" />
+            <img src={wicon} height={70} />
           </div>
           <div className="weather-temp text-center">
             <p>
@@ -97,7 +97,7 @@ const Weather = () => {
           <div className="data-container">
             <div className="element">
               <img
-                src="./weather-app/assets/icons8-humidity.gif"
+                src="https://github.com/Elmeliani-y/Weather_app/tree/main/src/weather-app/assets/icons8-humidity.gif"
                 alt="Humidity Icon"
               />
               <div className="data">
@@ -108,7 +108,7 @@ const Weather = () => {
             </div>
             <div className="element">
               <img
-                src="./weather-app/assets/icons8-wind.gif"
+                src="https://github.com/Elmeliani-y/Weather_app/tree/main/src/weather-app/assetsicons8-wind.gif"
                 alt="Wind Icon"
               />
               <div className="data">
